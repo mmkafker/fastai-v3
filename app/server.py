8e68e59b68fd8aa969fb32f39e744b4be5b4cf31
Matt Kafker <mmkafker@gmail.com>
2019-10-03 20:13:12 -0700

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1Yn2i8Ylmkp_VUEYI2QGiSBqLJyWVx0ja'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['apples','bananas']
 path = Path(__file__).parent
 
 app = Starlette()
